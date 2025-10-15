98ee5707ff7cdd3bc2eb1fedffc2218ffa2cc6e2
IanMacKenzie <26mackenziei57@stu.smuhsd.org>
2025-10-14 21:35:01 -0700

    Initial Commit

---
+++ PartnerLab2/PartnerLab2App.swift
@@ -0,0 +1,17 @@
+//
+//  PartnerLab2App.swift
+//  PartnerLab2
+//
+//  Created by cisstudent on 10/14/25.
+//
+
+import SwiftUI
+
+@main
+struct PartnerLab2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PartnerLab2Tests/PartnerLab2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  PartnerLab2Tests.swift
+//  PartnerLab2Tests
+//
+//  Created by cisstudent on 10/14/25.
+//
+
+import Testing
+@testable import PartnerLab2
+
+struct PartnerLab2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
